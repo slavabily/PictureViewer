856d983768665ad10c1f9abfc8588f10b6a69327
slavabily <slavabily@me.com>
2020-01-20 18:09:37 +0200

    Initial Commit

---
+++ PictureViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PictureViewer
+//
+//  Created by slava bily on 20/1/20.
+//  Copyright © 2020 slava bily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
